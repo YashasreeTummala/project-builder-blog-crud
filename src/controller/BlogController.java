09d76315a3415b881609cf83926e80a371ae5455
yashasree <yashasreeaditya@gmail.com>
2020-05-15 16:25:25 +0530

    FSJ0420A105

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.Blog;
 import model.User;
 import service.CRUDOperations;
-import service.ExcelFileStorage;
+//import service.ExcelFileStorage;
 import utility.CheckBlogPost;
 
 
@@ -61,11 +62,16 @@ public class BlogController extends HttpServlet {
 		
 		CRUDOperations crud=new CRUDOperations();
 		List<Blog> listblog = crud.createBlog(blog);
+		
+		ArrayList<Blog> crudOp = new ArrayList<Blog>();
+		
+		Object titleSort=crud.sortByTitle( crudOp);
+		Object dateSort=crud.sortByDate( crudOp);	
 
 		if(check) {
 			request.setAttribute("listBlog", listblog);
-//			request.setAttribute("blog", blog);
-//			request.setAttribute("user",user);
+			request.setAttribute("blog", blog);
+			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
--- src/service/CRUDOperations.java
+++ src/service/CRUDOperations.java
@@ -1,29 +1,42 @@
 package service;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import model.Blog;
-
 public class CRUDOperations {
 	static List<Blog> list=new ArrayList<Blog>(); // Store the blog in the list
 	
 	public List<Blog> createBlog(Blog blog) throws IOException {
 		list.add(blog);
+		System.out.println("New Blog: "+ list);
 		return list;
 	}
 	
 	public List<Blog> viewBlog(Blog blog) throws IOException{
-		return null;
+		return list;
 		
 	}
 	public List<Blog> updateBlog(Blog blog) throws IOException{
-		return null;
+		list.set(list.indexOf(blog), blog);
+		return list;
 	
 	}
 	public List<Blog> deleteBlog(Blog blog) throws IOException{
-		return null;
-		
+		 list.remove(blog);
+	      System.out.println("\nAfter removing: "+list);
+		return list;
 	}
+	public Object sortByTitle(List<Blog> blog) {
+		ArrayList<Blog> sortByTitle=new ArrayList<>();	
+		sortByTitle.sort((t1,t2) -> t1.getBlogTitle().compareTo(t2.getBlogTitle()));
+		System.out.println("\nSort by Title: "+sortByTitle);
+		return sortByTitle;
+		}
+	
+	public Object sortByDate(List<Blog> blog) {
+		ArrayList<Blog> sortByDate=new ArrayList<>();	
+		sortByDate.sort((d1,d2) -> d1.getDate().compareTo(d2.getDate()));
+		System.out.println("\nSort by Date: "+sortByDate);
+		return sortByDate;
+		}
 }
